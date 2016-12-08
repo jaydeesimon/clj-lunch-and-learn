f26eb7dd7f7347e4c4b84b7b72eeb972536b0ada
Andrew Zirm <andrewzirm@gmail.com>
2016-12-08 09:21:42 -0500

    Add rhizome graphviz

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [rhizome "0.2.7"]
+                 [com.datomic/datomic-free "0.9.5544"]
+                 [com.datomic/clj-client "0.8.606"]
+                 [camel-snake-kebab "0.4.0"]]
   :main ^:skip-aot clojure-lunch-and-learn.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
